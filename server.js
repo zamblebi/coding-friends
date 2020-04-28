c93ffdb31aa3737a0f32dd7c6da62fc6e3a5b75d
Bouye Zamble Bi Samuel <bouyesamuel@gmail.com>
2020-04-28 16:44:43 +0000

    arrenge folder and files creation of basic layout

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require('express')
 const app = express();
+const path = require('path')
 
 const ejs = require('ejs')
 
+//use app
+
+app.use(express.static('public'))
+
+
+
 //set up view engin ejs
 app.set('view engine', 'ejs')
-
+app.set('views', path.join(__dirname,'views'))
 
 //route 
+// var title = 'Coding Friends Forum'
 app.get('/', function(req,res){
-    res.render('pages/index')
+    res.render('pages/index', {title:'Coding'})
 })
 
 app.get('/support', function(req,res){
-    res.render('pages/support')
+    res.render('pages/support', {title: 'Support'})
 })
 
 app.listen(3000,function(){
